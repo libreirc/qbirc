2835af2d763a98ab5ac5c17e8845f2143c21931c
Hyeon Kim <simnalamburt@gmail.com>
2016-04-04 22:48:39 +0900

    Refine the error message

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ Client.prototype.__proto__ = Emitter.prototype;
 
 Client.prototype.write = function(str, fn) {
   if (str.indexOf('\n') != -1 || str.indexOf('\r') != -1) {
-    fn && fn(new Error('Newline character was used in the message'));
+    fn && fn(new Error("You should not pass any '\\n' or '\\r' to `write()`."));
     return;
   }
   this.stream.write(str + '\r\n', fn);
